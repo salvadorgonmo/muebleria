0c7c0b0354f33aa8fe49ffe8deddc772f9e493cf
salvador.gonzalez <salvador.gonzalez@unosquare.com>
2019-10-12 15:11:07 -0500

    adding responsive web menu dropdown

--- client/src/components/common/Menu/index.js
+++ client/src/components/common/Menu/index.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Menu.css'
+
+const handleResponsive = (responsive, setClass) => {
+  if (responsive === 'responsive') {
+    setClass('')
+  } else {
+    setClass('responsive')
+  }
+}
 
 const Menu = ({
   Link
 }) => {
-
+  const [ responsiveClass, setClass ] = useState('')
   return (
     <React.Fragment>
-      <div className={styles.container}>
+      <div className={`${styles['menu-container']} ${styles[responsiveClass]}`}>
         <a>
-          <Link to='/' className={styles.products}> Menu principal </Link>
+          <Link to='/' className={styles['menu-item']}> 
+            <i className={`fas fa-hammer ${styles.icon}`}></i>FurnitureStore
+          </Link>
+          <div className={`fa fa-bars ${styles['hamburguer-icon']}`} onClick={() => handleResponsive(responsiveClass, setClass)}></div>
         </a>
         <a>
-          <Link to='/usuarios'> Usuarios </Link>
-        </a>
-        <a>
-          <Link to='/productos'> Productos </Link>
-        </a>
-        <a>
-          <Link to='/categorias'> Categorias </Link>
-        </a>
-        <a>
-          <Link to='/clientes'> Clientes </Link>
+          <Link to='/usuarios' className={styles['menu-item']}>
+          <i className={`fas fa-user ${styles.icon}`}></i> Usuarios </Link>
+          <Link to='/productos' className={styles['menu-item']}> 
+          <i className={`fas fa-box ${styles.icon}`}></i>Productos </Link>
+          <Link to='/categorias' className={styles['menu-item']}> 
+          <i className={`fas fa-folder-minus ${styles.icon}`}></i>Categorias </Link>
+          <Link to='/clientes' className={styles['menu-item']}> 
+          <i className={`fas fa-users ${styles.icon}`}></i>Clientes </Link>
         </a>
       </div>
     </React.Fragment>
